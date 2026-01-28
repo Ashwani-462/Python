a5f85a5280bc24411d4055536f9ac8355c2155f6
Ashwani-462 <ashwanim1198@gmail.com>
2026-01-28 06:57:28 +0530

    DL eligibility
    Using if-elif chech user is eligibile for DL pr not.

---
+++ For_DL.py
@@ -0,0 +1,7 @@
+age=int(input("Enter your age: "))
+if(age>18):
+    print("You are eligible for DL.")
+elif(age<18):
+    print("You are not eligible for DL.")
+else:
+    print("You are eligible for DL with parental consent.")
