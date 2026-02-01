f2c50124430f950275460cb8c30e0ec036925c6e
Ashwani-462 <ashwanim1198@gmail.com>
2026-02-01 12:14:57 +0530

    sub with marks as dict type

---
+++ sub_marks.py
@@ -0,0 +1,8 @@
+marks={}
+x=int(input("Enter phy marks: "))
+marks.update({'phy':x})
+y=int(input("Enter chem marks: "))
+marks.update({'chem':y})
+z=int(input("Enter math marks: "))  
+marks.update({'math':z})
+print(marks)
