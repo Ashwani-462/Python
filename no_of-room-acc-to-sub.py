e3c4dcc728dadf76757bc53e422d01b0c5b1328f
Ashwani-462 <ashwanim1198@gmail.com>
2026-02-01 12:10:39 +0530

    Add classroom count based on subjects
    Calculate and display the number of classrooms based on subjects.

---
+++ no_of-room-acc-to-sub.py
@@ -0,0 +1,5 @@
+#Finding no of classroom according to subject
+sub={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(sub)
+room=(len(sub))
+print("There are ",room," no of room according to subject")
